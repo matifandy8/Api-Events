50460c6b8fdd052bee8b112fe426a21e08fe7039
matias <matifandy@gmail.com>
2023-09-04 14:28:40 -0300

    fix: fix cors problem

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
-const port = process.env.PORT = 8080
+const port = process.env.PORT = 8081
 const secretKey = process.env.SECRET_PASS
+const cors = require('cors')
+
 const fs = require('fs')
 
 const app = express()
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:3000'
+    ]
 
-app.get('/', function (req, res) {
-  const { ticker } = req.params
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+
+    if (!origin) {
+      return callback(null, true)
+    }
+
+    return callback(new Error('Not allowed by CORS'))
+  }
+}))
+app.disable('x-powered-by')
+
+app.get('/events', function (req, res) {
   const { key } = req.query
 
-  if (!ticker || !key || key !== secretKey) {
-    return res.status(400).send({ message: 'Please provide api key and ticker' })
+  if (key !== secretKey) {
+    return res.status(400).send({ message: 'Please provide api key' })
   }
-
-  const url = process.env.URL
 
   async function scrapeData () {
     try {
-      const { data } = await axios.get(url)
+      const { data } = await axios.get('https://www.sportstourismnews.com/sports-events-calendar-uk-global-sporting-events/')
       const $ = cheerio.load(data)
       const extractEventInfo = (element) => {
         const event = {
           name: '',
           date: ''
         }
-        event.date = element.find('div.tribe-events-widget-events-list__event-wrapper.tribe-common-g-col > article > div > header > div').text()
+        const month = element.find('div.tribe-events-widget-events-list__event-date-tag.tribe-common-g-col > time > span.tribe-events-widget-events-list__event-date-tag-month').text()
+          .trim()
+        const day = element.find('div.tribe-events-widget-events-list__event-date-tag.tribe-common-g-col > time > span.tribe-events-widget-events-list__event-date-tag-daynum.tribe-common-h2.tribe-common-h4--min-medium').text()
           .trim()
         event.name = element.find('h3').text()
           .trim()
+        event.date = `${day} ${month}`
+
         return event
       }
       const listItems = $('#secondary > div.tribe-compatibility-container > div > div > div.tribe-events-widget-events-list__events > div')
